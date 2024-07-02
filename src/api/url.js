240d16e39cd3245f36e7b88aa1ab9e254e9a1d86
ab1123 <asarma626@gmail.com>
2024-07-02 13:43:52 +0530

    prod: Responsiveness

--- src/api/url.js
+++ src/api/url.js
@@ -1,2 +1,2 @@
-export const URL="http://localhost:5000/api"
-// export const URl="https://api.tcx.academy/api"
+// export const URL="http://localhost:5000/api"
+export const URl="https://api.tcx.academy/api"

71ed28a135d1f0f4d3b2ee153c38cad48659de0d
ab1123 <asarma626@gmail.com>
2024-07-15 15:50:09 +0530

    chores: bug fixes: 1. Fixed the Already Submitted Error in the projects Page

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,42 +1,59 @@
 import React, { useEffect, useState } from "react";
 import Split from "react-split";
 import Navbar from "./Navbar";
+// import { toast } from "react-toastify";
 import { getProjectByPid } from "../api/axios";
 import { useParams } from "react-router-dom";
 import { useUser } from "../store/UserContext";
 import { submitProject } from "../api/axios";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import BouncingDotsLoader from "./Loaders/Bouncing";
 
 const Editor = () => {
   const [htmlContent, setHtmlContent] = useState("");
   const { pid } = useParams();
+  const [loading, setLoading] = useState(true)
   const [link, setLink] = useState("");
   const [gradioLink, setGradioLink] = useState("");
-  const { user } = useUser();
+  const { user, fetchUserDetails } = useUser();
   const [submitted, setSubmitted] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
+    window.addEventListener("beforeunload", handleBeforeUnload);
     getProjectByPid(pid).then((response) => {
       setHtmlContent(response.description);
       setLink(response.link);
     });
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
   }, [pid]);
 
 
   useEffect(() => {
-    function checkSubmit() {
-      const completedProject = user.projects.find((project) => project.pid === pid);
+    async function checkSubmit() {
+      setLoading(true);
+      // Make sure this function handles state properly
 
-      if (completedProject) {
-        setSubmitted(true)
-      } else {
-        setSubmitted(false)
-      }
+      const isSubmitted = user.projects.includes(pid);
+      setSubmitted(isSubmitted);
+      setLoading(false);
+
     }
-  }, [])
 
+    if (user) {
+      checkSubmit();
+    }
+  }, [user]);
+
+  function handleBeforeUnload(e) {
+    e.preventDefault(); // Prevent the default behavior
+    e.returnValue = 'Are you sure you want to leave? Changes you made may not be saved.'; // Standard message does not display on some browsers
+    return 'Are you sure you want to leave? Changes you made may not be saved.';
+  }
 
   const handleSubmit = async () => {
     // Example Gradio link to be validated
@@ -65,7 +82,7 @@ const Editor = () => {
 
           } else {
             // console.log("Invalid Link: Link already exists in the database, shows the link is copied");
-            toast.error("Uh Oh!! Invalid Link")
+            toast.error("Link Already Submitted!!!!")
           }
         } catch (error) {
           console.error("Error submitting project:", error);
@@ -81,53 +98,55 @@ const Editor = () => {
 
   return (
     <>
-      <div className="flex items-center justify-center w-full">
-        <Navbar />
-      </div>
-      <div className="flex justify-center items-center h-screen bg-black text-white">
-        <Split
-          sizes={[50, 50]}
-          minSize={100}
-          expandToMin={false}
-          gutterSize={10}
-          gutterAlign="center"
-          snapOffset={30}
-          dragInterval={1}
-          direction="horizontal"
-          cursor="col-resize"
-          className="flex w-full h-full"
-        >
-          <div className="flex flex-1 flex-col bg-gray-900 p-4 overflow-auto">
-            <div
-              className="text-white mt-20 h-[80vh] overflow-y-scroll"
-              dangerouslySetInnerHTML={{ __html: htmlContent }}
-            />
-            {submitted ? <div className="flex flex-col justify-start items-start">
-              <div className="flex justify-between gap-6">
-                <label className=" text-white text-left rounded-xl p-4 text-xl" htmlFor="gradioLink">Submit Your Gradio Link here :</label>
-                <input id="gradioLink" name="gradioLink" className="p-2 rounded-md bg-[#121418] text-white outline-none focus:ring-2 focus:ring-[#FF7C1D] placeholder:text-gray-700" type="text" value={gradioLink} onChange={(e) => setGradioLink(e.target.value)} />
-                <button className="px-6 py-2 rounded-md bg-[#FF7C1D] text-white text-lg focus:outline-none hover:bg-[#FF6818] transition duration-200" onClick={handleSubmit}>Submit</button>
-              </div>
-            </div> : <p className="text-red-400 text-2xl self-center">*You have already Submitted the project</p>}
-          </div>
-          <div className="flex flex-1 flex-col items-center  justify-center p-4">
-            <iframe
-              src={link}
-              style={{
-                width: "100%",
-                height: "85vh",
-                border: 0,
-                borderRadius: "4px",
-                overflow: "hidden",
-                marginTop: "10vh",
-              }}
-              title="React Counter Example"
-              allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-              sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-            ></iframe>
-          </div>
-        </Split>
-      </div>
+      {loading ? <BouncingDotsLoader /> : <>
+        <div className="flex items-center justify-center w-full">
+          <Navbar />
+        </div>
+        <div className="flex justify-center items-center h-screen bg-black text-white">
+          <Split
+            sizes={[50, 50]}
+            minSize={100}
+            expandToMin={false}
+            gutterSize={10}
+            gutterAlign="center"
+            snapOffset={30}
+            dragInterval={1}
+            direction="horizontal"
+            cursor="col-resize"
+            className="flex w-full h-full"
+          >
+            <div className="flex flex-1 flex-col bg-gray-900 p-4 overflow-auto">
+              <div
+                className="text-white mt-20 h-[80vh] overflow-y-scroll"
+                dangerouslySetInnerHTML={{ __html: htmlContent }}
+              />
+              {!submitted ? <p className="text-red-400 text-2xl self-center">*You have already Submitted the project</p> : <div className="flex flex-col justify-start items-start">
+                <div className="flex justify-between gap-6">
+                  <label className=" text-white text-left rounded-xl p-4 text-xl" htmlFor="gradioLink">Submit Your Gradio Link here :</label>
+                  <input id="gradioLink" name="gradioLink" className="p-2 rounded-md bg-[#121418] text-white outline-none focus:ring-2 focus:ring-[#FF7C1D] placeholder:text-gray-700" type="text" value={gradioLink} onChange={(e) => setGradioLink(e.target.value)} />
+                  <button className="px-6 py-2 rounded-md bg-[#FF7C1D] text-white text-lg focus:outline-none hover:bg-[#FF6818] transition duration-200" onClick={handleSubmit}>Submit</button>
+                </div>
+              </div>}
+            </div>
+            <div className="flex flex-1 flex-col items-center  justify-center p-4">
+              <iframe
+                src={link}
+                style={{
+                  width: "100%",
+                  height: "85vh",
+                  border: 0,
+                  borderRadius: "4px",
+                  overflow: "hidden",
+                  marginTop: "10vh",
+                }}
+                title="React Counter Example"
+                allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+                sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+              ></iframe>
+            </div>
+          </Split>
+        </div>
+      </>}
     </>
   );
 };

8e0f75962979eb02a2c63605a1bf0614140d5ae4
ab1123 <asarma626@gmail.com>
2024-07-17 12:46:40 +0530

    jjbkfr

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -474,7 +474,7 @@ function Hero() {
       </div> */}
 
       <Bento />
-      
+
 
       <SliderSection />
 
@@ -548,9 +548,9 @@ function Hero() {
 
         </div>
       </div>
-      <div className='dynamicBG' style={{ backgroundImage: `url(${bg3})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-          <Footercard />
-        </div>
+      <div  style={{ backgroundImage: `url(https://images.unsplash.com/photo-1718027808460-7069cf0ca9ae?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+        <Footercard />
+      </div>
       <Footer />
     </>
   );

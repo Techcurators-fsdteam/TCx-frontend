06a729e17bc5748aac3ee682e93d8005b8c47996
ab1123 <asarma626@gmail.com>
2024-07-17 12:32:38 +0530

    URL changes

--- src/api/url.js
+++ src/api/url.js
@@ -1,5 +1,5 @@
-export const URL = "http://localhost:5000/api";
+// export const URL = "http://localhost:5000/api";
 
-// export const URL= "https://api.tcx.academy/api";
+export const URL= "https://api.tcx.academy/api";
 
 
